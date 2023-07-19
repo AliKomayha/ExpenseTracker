60ab81298d4e5c87b82f7cbae71ab2ef3313c9e7
alli komayha <Ali Komayha>
2023-07-19 16:31:41 +0300

    adding total summation function

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 
 $(document).ready(function() {
 
+    let totalAmount = 0; //
     $("form").submit(function(event) {
       event.preventDefault(); // Prevent form submission
   
       const expenseName = $("#name-input").val();
-      const expenseAmount = $("#amount-input").val();
+      const expenseAmount = parseFloat($("#amount-input").val());//
 
-      if (expenseName.trim() !== ""  && expenseAmount.trim() !== "") {
+      if (expenseName.trim() !== ""  &&  !isNaN(expenseAmount)) {
         const totalRow = $("#expenses tr:last");
         const newExpense = $("<tr><td></td><td></td></tr>");
         
         newExpense.find("td").eq(0).text(expenseName);
-        newExpense.find("td").eq(1).text(expenseAmount);
+        newExpense.find("td").eq(1).text(expenseAmount.toFixed(2));
         totalRow.before(newExpense);
         
+        totalAmount += expenseAmount; // Update total amount
+        $("#total").text(totalAmount.toFixed(2));
+
         $("#name-input").val("");
         $("#amount-input").val("");
       }
